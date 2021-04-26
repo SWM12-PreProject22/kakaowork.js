0ceab59aa25f23b5021a0e3d54ba544aacce24cb
Eunhak Lee <lee@enak.kr>
2021-04-27 06:40:49 +0900

    fix: SelectBlock 의 serialize 할 때 name 을 빼먹음

--- lib/models/blocks/modal/SelectBlock.ts
+++ lib/models/blocks/modal/SelectBlock.ts
@@ -4,10 +4,15 @@ import InputBlock from './InputBlock';
 export default class SelectBlock extends InputBlock {
   options: SelectOption[];
 
-  constructor(name: string, options: SelectOption[], required?: boolean, placeholder?: string) {
+  constructor(
+    name: string,
+    options: SelectOption[],
+    required?: boolean,
+    placeholder?: string
+  ) {
     super(name, required, placeholder);
     this.options = new Array<SelectOption>();
-    options.forEach(o => this.options.push(o));
+    options.forEach((o) => this.options.push(o));
   }
 
   type(): string {
@@ -17,9 +22,14 @@ export default class SelectBlock extends InputBlock {
   serialize(): Map<string, any> {
     const map = new Map();
     map.set('type', this.type());
+    map.set('name', this.name);
     if (this.required !== undefined) map.set('required', this.required);
-    if (this.placeholder !== undefined) map.set('placeholder', this.placeholder);
-    map.set('options', this.options.map(o => o.serialize()));
+    if (this.placeholder !== undefined)
+      map.set('placeholder', this.placeholder);
+    map.set(
+      'options',
+      this.options.map((o) => Object.fromEntries(o.serialize()))
+    );
 
     return map;
   }
